304643c02d960ad26cf1906d2d4fbd68fdc111a5
sulinwei <1917488688@qq.com>
2020-05-17 22:20:39 +0800

    fix the primary key cannot add

--- src/main/resources/db/migration/V1__Create_person_table.sql
+++ src/main/resources/db/migration/V1__Create_person_table.sql
@@ -2,14 +2,14 @@
 SET search_path TO s_user;
 
 create table s_user.t_person (
-    id int not null primary key ,
+    id serial primary key  ,
     name varchar(20) not null,
     phone varchar(11),
     relationship_id int[]
 );
 
 create table s_user.t_relationship(
-    id int not null primary key ,
+    id serial primary key ,
     relationship_type int not null,
     person_one int not null references s_user.t_person(id),
     person_two int not null references s_user.t_person(id),
