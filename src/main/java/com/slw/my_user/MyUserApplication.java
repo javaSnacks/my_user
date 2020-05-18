6ad3bfa5dfcb720302b97df0eab2ce994b289faf
sulinwei <sulinwei@xinzhili.cn>
2020-05-18 17:58:46 +0800

    test conflicts 2

--- src/main/java/com/slw/my_user/MyUserApplication.java
+++ src/main/java/com/slw/my_user/MyUserApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class MyUserApplication {
 
 	public static void main(String[] args) {
+        System.out.println(2);
 		SpringApplication.run(MyUserApplication.class, args);
 	}
 
